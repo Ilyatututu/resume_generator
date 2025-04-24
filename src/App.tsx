6ef7e36f5d115bd97a36404c745a24f164b36afa
Ilya <ilya.voster@yandex.ru>
2025-04-24 21:23:33 +0300

    maybe finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react';
 import bridge, { UserInfo } from '@vkontakte/vk-bridge';
 import { View, SplitLayout, SplitCol } from '@vkontakte/vkui';
-import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
 import { ResumeBuilder } from './panels/ResumeBuilder';
-import { Home } from './panels';
-import { DEFAULT_VIEW_PANELS } from './routes';
+
 
 export const App = () => {
-  const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
   const [fetchedUser, setUser] = useState<UserInfo | undefined>();
 
   useEffect(() => {
